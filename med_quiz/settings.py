7e932384fda14d31402ff78fb368ebc56a61086c
velikhanov <velikhanov@users.noreply.github.com>
2026-02-14 02:46:27 +0300

    added favicon

--- med_quiz/settings.py
+++ med_quiz/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'med_quiz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
