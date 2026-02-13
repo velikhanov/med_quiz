11b86fbfb72a014bc5c73f9cc498d3dee5fad6ce
velikhanov <velikhanov@users.noreply.github.com>
2026-02-14 02:01:45 +0300

    wsgi setting update

--- med_quiz/wsgi.py
+++ med_quiz/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+path = '/home/velikhanov/med_quiz'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'med_quiz.settings')
 
