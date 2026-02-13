c949a55867aa6b26b8027bcd9a3a2bcde04289ef
velikhanov <velikhanov@users.noreply.github.com>
2026-02-14 01:46:11 +0300

    added default auto field

--- med_quiz/settings.py
+++ med_quiz/settings.py
@@ -100,6 +100,7 @@ else:
         }
     }
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
