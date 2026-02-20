e89d3fbdd4b0073bd7c01d605e7e5cc5bd257aba
velikhanov <velikhanov@users.noreply.github.com>
2026-02-20 22:35:03 +0300

    remove some admin actions from non superusers

--- apps/content/admin.py
+++ apps/content/admin.py
@@ -43,10 +43,8 @@ class PDFUploadAdmin(admin.ModelAdmin):
         actions = super().get_actions(request)
 
         if not request.user.is_superuser:
-            if 'process_batch_5' in actions:
-                del actions['process_batch_5']
-            if 'process_batch_10' in actions:
-                del actions['process_batch_10']
+            for action in ('process_batch_5', 'process_batch_10', 'reset_pdf_status'):
+                actions.pop(action, None)
 
         return actions
 
